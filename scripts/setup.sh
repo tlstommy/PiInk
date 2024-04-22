f3f12cd4b59bfee3125299281146e07035f07967
Thomas Smith <36305669+tlstommy@users.noreply.github.com>
2024-04-22 15:56:20 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,10 +16,12 @@ enable_interfaces(){
   #enable spi
   sudo sed -i 's/^dtparam=spi=.*/dtparam=spi=on/' /boot/config.txt
   sudo sed -i 's/^#dtparam=spi=.*/dtparam=spi=on/' /boot/config.txt
+  sudo raspi-config nonint do_spi 0
   print_success "SPI Interface has been enabled."
   #enable i2c
   sudo sed -i 's/^dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
   sudo sed -i 's/^#dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
+  sudo raspi-config nonint do_i2c 0
   print_success "I2C Interface has been enabled.\n"
 }
 
