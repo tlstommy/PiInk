365e164bfe49f39a38c48eabfa5ec8d7bbeee5d7
tlstommy <tls.tommy11@gmail.com>
2025-01-19 14:07:24 -0500

    think i fixed start script issue

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-source piinkenv/bin/activate
-pid=$(lsof -i :80| awk '/python/ { pid=$2 } END { print pid }')
+date
+
+#kill any existing procs
+pid=$(sudo netstat -tuln | grep ':80 ' | awk '{print $7}' | cut -d'/' -f1)
+if [[ -n "$pid" ]]; then
+    echo "Found process using port 80: PID $pid."
+    echo "Killing process $pid..."
+    sudo kill -9 "$pid" && echo "Process $pid killed." || echo "Failed to kill process $pid."
+else
+    echo "No process found using port 80!"
+fi
+
+#activate venv
+if [[ -d "piinkenv" ]]; then
+    source piinkenv/bin/activate
+    if [[ $? -ne 0 ]]; then
+        echo "[ERROR]: Cant activate venv: piinkenv."
+        exit 1
+    fi
+else
+    echo "[ERROR]: Cant find venv: piinkenv."
+    exit 1
+fi
+
 currentDir=$(pwd)
 currentFolder=${PWD##*/} 
 
@@ -10,6 +32,7 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+#dir check
 if [ "$currentFolder" == "scripts" ]; then
   cd ..
   currentDir=$(pwd)
@@ -35,6 +58,7 @@ if [[ -d "piinkenv" ]]; then
     echo "[ERROR]: Failed to activate the virtual environment. Please check that the venv is setup."
     exit 1
   fi
+  
   sudo python "$currentDir/src/webserver.py"
 else
   echo "[ERROR]: Virtual environment 'piinkenv' not found. Please ensure the virtual environment is set up correctly."
